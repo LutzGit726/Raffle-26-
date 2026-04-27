10871b8bb3b653c027714466df498ae0ed7189f1
LutzGit726 <lutzr411@gmail.com>
2026-04-27 14:37:45 -0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import RaffleApp from './RaffleApp'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RaffleApp />
-  </React.StrictMode>
-)
